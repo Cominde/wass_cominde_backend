0d8c0ee3764a50da09a8891de81b9b4f97c1b7ce
joeshwoa.george <joeshwoa.george@gmail.com>
2025-02-11 00:09:51 +0000

    change connection string in database

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose =require ('mongoose');
 
 const dbConnection = () =>{
     
-mongoose.connect(process.env.DB_URI).then((conn)=>{
+mongoose.connect(process.env.MONGO_URL).then((conn)=>{
     console.log(`database connected :${conn.connection.host}`)
     
     }).catch((error)=>{
