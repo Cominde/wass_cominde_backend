0838c8335ac488da1f935e402ef0336d93b7764b
joeshwoa.george <joeshwoa.george@gmail.com>
2025-02-11 00:04:54 +0000

    change dbconnection for messaging service

--- services/messagingServices.js
+++ services/messagingServices.js
@@ -12,9 +12,26 @@ const mongoose = require("mongoose");
 const { Client, RemoteAuth, MessageMedia } = require("whatsapp-web.js");
 const { MongoStore } = require("../mongo-store-edited");
 const QRCode = require("qrcode");
+const mongoose =require ('mongoose');
 const { dbConnection } = require("../config/database"); // Assuming you have a file named dbConnection.js that exports mongooseConnection
 let store;
 let client;
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log(formatDate(new Date())+': DATABASE CONNECTED');
+  store = new MongoStore({ mongoose: mongoose });
+
+  client = new Client({
+      clientId: 'main',
+      authStrategy: new RemoteAuth({
+          store: store,
+          backupSyncIntervalMs: 300000
+      }),
+      puppeteer: {
+          args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      }
+  });
+});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -67,25 +84,6 @@ mailOptions = function (text) {
 const qrCodePath = path.join(__dirname, "qr_code.png");
 
 exports.startSession = async (req, res, next) => {
-  try {
-    await dbConnection;
-    console.log(formatDate(new Date()) + ": DATABASE CONNECTED");
-    store = new MongoStore({ mongoose: mongoose, path: "/tmp/" });
-
-    client = await new Client({
-      clientId: "main",
-      authStrategy: new RemoteAuth({
-        store: store,
-        backupSyncIntervalMs: 300000,
-      }),
-      puppeteer: {
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      },
-    });
-    console.log("done");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-  }
   try {
     let newSession = false;
 
